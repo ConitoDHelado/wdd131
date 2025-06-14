879cfa513c2355f347e62d7b43bdd20f74982d64
ConitoDHelado <eprezedelgado@byupathway.edu>
2025-06-15 01:52:22 +0200

    fixed bug

--- project/scripts/form2.js
+++ project/scripts/form2.js
@@ -11,13 +11,13 @@ button.addEventListener("click", function () {
 
         if (input.type === "text" || input.type === "tel" || input.type === "email" || input.type === "date") {
             
-            let label = getLabelText
-            formArray.push({ label: labelText, value: input.value });
+            let labelText = getLabelText(input);
+            formArray.push({label: labelText, value: input.value });
             setFormArray();
         } else if (input.type === "radio" && input.checked) {
 
-            let label = get
-            formArray.push(input.value);
+            let labelText = getGroupLabel(input);
+            formArray.push({label: labelText, value: input.value });
             setFormArray();
         }
     })
@@ -26,7 +26,14 @@ button.addEventListener("click", function () {
 function getLabelText(input) {
     // Try using <label for="inputId">
     let label = document.querySelector(`label[for="${input.id}"]`);
-    label.innerText.trim();
+    return label.innerText.trim();
+}
+
+function getGroupLabel(input) {
+    let groupContainer = input.closest("div");
+    return groupContainer.textContent
+
+
 }
 
 function setFormArray() {
